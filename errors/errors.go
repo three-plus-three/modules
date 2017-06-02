4319f4c620a8960988b5063f0c00d3d4056530f5
meifakun <runner.mei@gmail.com>
2017-06-02 12:12:55 +0800

    add errors

--- errors/errors.go
+++ errors/errors.go
@@ -1,6 +1,8 @@
 package errors
 
 import (
+	"bytes"
+	native "errors"
 	"fmt"
 	"net/http"
 )
@@ -27,4 +29,54 @@ func (err *ErrNotFound) Error() string {
 
 func NotFound(id interface{}) RuntimeError {
 	return &ErrNotFound{id: id}
+}
+
+func New(msg string) error {
+	return native.New(msg)
+}
+
+type PGError interface {
+	Error() string
+	Fatal() bool
+	Get(k byte) (v string)
+}
+
+func Wrap(s string, e error) error {
+	if "" == s {
+		return e
+	}
+	return native.New(s + ": " + e.Error())
+}
+
+type MutiErrors struct {
+	msg  string
+	errs []error
+}
+
+func (self *MutiErrors) Error() string {
+	return self.msg
+}
+func (self *MutiErrors) Errors() []error {
+	return self.errs
+}
+
+func Concat(msg string, errs []error) error {
+	if len(errs) == 1 {
+		return errs[0]
+	}
+	var buffer bytes.Buffer
+	isFirst := true
+	if msg != "" {
+		isFirst = false
+		buffer.WriteString(msg)
+	}
+	for _, e := range errs {
+		if isFirst {
+			isFirst = false
+		} else {
+			buffer.WriteString("\n ")
+		}
+		buffer.WriteString(e.Error())
+	}
+	return &MutiErrors{msg: buffer.String(), errs: errs}
 }
