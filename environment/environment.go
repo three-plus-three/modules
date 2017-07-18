aadf964efe2be3b500ba961135ee6623d7e1131f
meifakun <runner.mei@gmail.com>
2017-07-18 11:31:35 +0800

    [environment] replace api

--- environment/environment.go
+++ environment/environment.go
@@ -218,13 +218,6 @@ func (self *Environment) GetEngineConfig() *EngineConfig {
 }
 
 func NewEnvironment(opt Options) (*Environment, error) {
-	if os.Getenv("tpt_version") == "3.2" {
-		return newEnvironment2(opt)
-	}
-	return newOldEnvironment(opt)
-}
-
-func newEnvironment2(opt Options) (*Environment, error) {
 	var fs *linuxFs
 	if runtime.GOOS == "windows" {
 		var rootDir string
@@ -305,78 +298,6 @@ func newEnvironment2(opt Options) (*Environment, error) {
 	if dataDir := os.Getenv("hw_data_dir"); dataDir != "" {
 		fs.dataDir = dataDir
 	}
-	return NewEnvironmentWithFS(fs, opt)
-}
-
-func newOldEnvironment(opt Options) (*Environment, error) {
-	var fs FileSystem
-	if runtime.GOOS == "windows" {
-		var rootDir string
-		if "" == opt.ConfDir {
-			if cwd, e := os.Getwd(); nil == e && FileExists(filepath.Join(cwd, "conf", "app.properties")) {
-				rootDir = cwd
-			} else if nil == e && FileExists(filepath.Join(cwd, "..", "conf", "app.properties")) {
-				rootDir = filepath.Clean(filepath.Join(cwd, ".."))
-			} else if exeDir, e := osext.ExecutableFolder(); nil == e && FileExists(filepath.Join(exeDir, "conf", "app.properties")) {
-				rootDir = exeDir
-			} else if nil == e && FileExists(filepath.Join(exeDir, "..", "conf", "app.properties")) {
-				rootDir = filepath.Clean(filepath.Join(exeDir, ".."))
-			} else if opt.IsTest {
-				rootDir, _ = os.Getwd()
-			} else {
-				found := false
-				for _, s := range []string{"../../../../cn/com/hengwei",
-					"../../../../../cn/com/hengwei",
-					"../../../../../../cn/com/hengwei",
-					"../../../../../../../cn/com/hengwei"} {
-					abs, _ := filepath.Abs(s)
-					abs = filepath.Clean(abs)
-					if DirExists(abs) {
-						rootDir = abs
-						found = true
-						break
-					}
-				}
-				if !found {
-					//if "<default>" == opt.rootDir { // "<default>" 作为一个特殊的字符，自动使用当前目录
-					if cwd, e := os.Getwd(); nil == e {
-						rootDir = cwd
-					} else {
-						rootDir = "."
-					}
-					// } else {
-					// 	return nil, errors.New("root directory is not found")
-					// }
-				}
-				opt.IsTest = true
-			}
-		} else {
-			if "<default>" == opt.ConfDir { // "<default>" 作为一个特殊的字符，自动使用当前目录
-				if cwd, e := os.Getwd(); nil == e {
-					rootDir = cwd
-				} else {
-					rootDir = "."
-				}
-			} else {
-				rootDir = filepath.Join(opt.ConfDir, "..")
-			}
-		}
-
-		fs = &winFs{
-			rootDir: rootDir,
-		}
-	} else {
-		fs = &linuxFs{
-			installDir: "/usr/local/tpt",
-			binDir:     "/usr/local/tpt/bin",
-			logDir:     "/var/log/tpt",
-			dataDir:    "/var/lib/tpt",
-			confDir:    "/etc/tpt",
-			tmpDir:     "/tmp/tpt",
-			runDir:     "/var/run/tpt",
-		}
-	}
-
 	return NewEnvironmentWithFS(fs, opt)
 }
 
