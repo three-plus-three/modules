b82ca31929813d6f468faa3480cee6d0ca8b65ba
meifakun <runner.mei@gmail.com>
2017-08-07 18:04:42 +0800

    add WatcherImpl

--- hub/engine/watch.go
+++ hub/engine/watch.go
@@ -1,10 +1,88 @@
 package engine
 
+import (
+	"sync/atomic"
+
+	"sync"
+)
+
 type Watcher interface {
 	OnNewQueue(name string)
 	OnRemoveQueue(name string)
 	OnNewTopic(name string)
 	OnRemoveTopic(name string)
+}
+
+type WatcherImpl struct {
+	mu           sync.Mutex
+	newQueues    atomic.Value
+	deleteQueues atomic.Value
+	newTopics    atomic.Value
+	deleteTopics atomic.Value
+}
+
+func (impl *WatcherImpl) add(value *atomic.Value, cb func(name string)) {
+	var funcs []func(name string)
+
+	if o := value.Load(); o != nil {
+		if fList, ok := o.([]func(name string)); ok {
+			funcs = make([]func(name string), len(fList), len(fList)+1)
+			copy(funcs, fList)
+		}
+	}
+
+	funcs = append(funcs, cb)
+	value.Store(funcs)
+}
+
+func (impl *WatcherImpl) call(value *atomic.Value, name string) {
+	if o := value.Load(); o != nil {
+		if fList, ok := o.([]func(name string)); ok {
+			for _, cb := range fList {
+				cb(name)
+			}
+		}
+	}
+}
+
+func (impl *WatcherImpl) ListenNewQueue(cb func(name string)) {
+	impl.mu.Lock()
+	defer impl.mu.Unlock()
+	impl.add(&impl.newQueues, cb)
+}
+
+func (impl *WatcherImpl) ListenRemoveQueue(cb func(name string)) {
+	impl.mu.Lock()
+	defer impl.mu.Unlock()
+	impl.add(&impl.deleteQueues, cb)
+}
+
+func (impl *WatcherImpl) ListenNewTopic(cb func(name string)) {
+	impl.mu.Lock()
+	defer impl.mu.Unlock()
+	impl.add(&impl.newTopics, cb)
+}
+
+func (impl *WatcherImpl) ListenRemoveTopic(cb func(name string)) {
+	impl.mu.Lock()
+	defer impl.mu.Unlock()
+	impl.add(&impl.deleteTopics, cb)
+}
+
+func (impl *WatcherImpl) OnNewQueue(name string) {
+	impl.call(&impl.newQueues, name)
+}
+
+func (impl *WatcherImpl) OnRemoveQueue(name string) {
+	impl.call(&impl.deleteQueues, name)
+}
+
+func (impl *WatcherImpl) OnNewTopic(name string) {
+	impl.call(&impl.newTopics, name)
+}
+
+func (impl *WatcherImpl) OnRemoveTopic(name string) {
+	impl.call(&impl.deleteTopics, name)
 }
 
 /*
