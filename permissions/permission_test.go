b81dad9320a87a9eb5eb28734348178e3a4295ee
meifakun <runner.mei@gmail.com>
2017-08-16 11:21:20 +0800

    fix unit test

--- permissions/permission_test.go
+++ permissions/permission_test.go
@@ -42,60 +42,60 @@ func TestHasPermission(t *testing.T) {
 		t.Error("except no p1 create")
 	}
 
-	if u.HasPermission("p12", CREATE) {
+	if !u.HasPermission("p12", CREATE) {
 		t.Error("except has p11 create")
 	}
 
 	//1个用户有1个角色 关联父子关系的两个权限组 操作相同 其权限相同
-	if u.HasPermission("p11", CREATE) {
+	if !u.HasPermission("p11", CREATE) {
 		t.Error("1个用户有1个角色 关联父子关系的两个权限组 操作相同 其权限相同" + "except has p31 create")
 	}
 
 	//1个用户有1个角色 关联父子关系的两个权限组 操作不相同 其权限相同
-	if u.HasPermission("p11", UPDATE) {
+	if !u.HasPermission("p11", UPDATE) {
 		t.Error("1个用户有1个角色 关联父子关系的两个权限组 操作不相同 其权限相同" + "except has p31 create")
 	}
 
 	//1个用户有1个角色 关联父子关系的两个权限组 操作不相同 其权限不相同 查父组权限
-	if u.HasPermission("p12", UPDATE) {
+	if !u.HasPermission("p12", UPDATE) {
 		t.Error("1个用户有1个角色 关联父子关系的两个权限组 操作不相同 其权限不相同 查父组权限")
 	}
 	//1个用户有1个角色 关联父子关系的两个权限组 操作不相同 其权限不相同 查子组权限
-	if u.HasPermission("p32", UPDATE) {
+	if !u.HasPermission("p32", UPDATE) {
 		t.Error("1个用户有1个角色 关联父子关系的两个权限组 操作不相同 其权限不相同 查子组权限")
 	}
 	u = readUser("t71")
 	//1个用户有1个角色 关联无上下关系两个权限组 操作不相同 其权限相同
-	if u.HasPermission("p12", UPDATE) {
+	if !u.HasPermission("p12", UPDATE) {
 		t.Error("1个用户有1个角色 关联无上下关系两个权限组 操作不相同 其权限相同")
 	}
 	u = readUser("t72")
 	//1个用户有2个角色  关联同一个权限组  操作不同
-	if u.HasPermission("p22", UPDATE) {
+	if !u.HasPermission("p22", UPDATE) {
 		t.Error("1个用户有2个角色  关联同一个权限组  操作不同")
 	}
 	//1个用户有2个角色  关联不同权限组  操作不同  两组权限相同
-	if u.HasPermission("p12", UPDATE) {
+	if !u.HasPermission("p12", UPDATE) {
 		t.Error("1个用户有2个角色  关联不同权限组  操作不同  两组权限相同")
 	}
 	//1个用户有2个角色  关联有父子关系权限组  操作不相同   两组权限相同
-	if u.HasPermission("p11", UPDATE) {
+	if !u.HasPermission("p11", DELETE) {
 		t.Error("1个用户有2个角色  关联有父子关系权限组  操作不相同   两组权限相同")
 	}
 
 	u = readUser("t73")
 	//1个用户有2个角色  角色1关联父子组  角色2关联父组     操作全不相同   父子组权限不相同 	查询的是父组权限
-	if u.HasPermission("p13", UPDATE) {
+	if !u.HasPermission("p13", UPDATE) {
 		t.Error("1个用户有2个角色  角色1关联父子组  角色2关联父组 操作全不相同   父子组权限不相同 	查询的是父组权限")
 	}
 
 	//1个用户有2个角色  角色1关联父子组  角色2关联子组     操作全不相同   父子组权限不相同    查询的是子组权限
-	if u.HasPermission("p32", UPDATE) {
+	if !u.HasPermission("p32", UPDATE) {
 		t.Error("1个用户有2个角色  角色1关联父子组  角色2关联父组 操作全不相同   父子组权限不相同 	查询的是父组权限")
 	}
 
 	//1个用户有2个角色  角色1关联父子组  角色2关联父组     操作全不相同   父子组权限相同
-	if u.HasPermission("p11", UPDATE) {
+	if !u.HasPermission("p11", UPDATE) {
 		t.Error("1个用户有2个角色  角色1关联父子组  角色2关联父组 操作全不相同   父子组权限不相同 	查询的是父组权限")
 	}
 
