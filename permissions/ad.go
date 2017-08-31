7c8e5e9ff403f43e14cfe2db88ef99ca4031a2bb
meifakun <runner.mei@gmail.com>
2017-08-31 18:02:47 +0800

    重构读 ldap 的代码

--- permissions/ad.go
+++ permissions/ad.go
@@ -1,63 +1,61 @@
 package permissions
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"strconv"
 	"strings"
 	"time"
+
+	"github.com/three-plus-three/modules/environment"
 
 	"gopkg.in/ldap.v2"
 )
 
 type ADConfig struct {
-	LdapServer string
-	LdapPort   string
-	Username   string
-	Password   string
-	BaseDN     string
+	Address   string
+	EnableTLS bool
+	Username  string
+	Password  string
+	BaseDN    string
 }
 
 //读取AD的配置
-func GetADConfig() (ADConfig, error) {
-	ad, err := ioutil.ReadFile("D:/go/HughRevel/src/web_example/conf/AD.json")
-	var adConfig ADConfig
-	if err != nil {
-		return adConfig, err
-	}
-	json.Unmarshal(ad, &adConfig)
-	return adConfig, nil
+func readLDAPConfig(env *environment.Environment) (ADConfig, error) {
+	ldapServer := env.Config.StringWithDefault("users.ldap_address", "")
+	ldapTLS := env.Config.BoolWithDefault("users.ldap_tls", false)
+
+	ldapUsername := env.Config.StringWithDefault("users.ldap_username", "")
+	ldapPassword := env.Config.StringWithDefault("users.ldap_password", "")
+	ldapDN := env.Config.StringWithDefault("users.ldap_dn", "")
+
+	return ADConfig{
+		Address:   ldapServer,
+		EnableTLS: ldapTLS,
+		Username:  ldapUsername,
+		Password:  ldapPassword,
+		BaseDN:    ldapDN,
+	}, nil
 }
 
-func GetUser() ([]User, error) {
+func ReadUserFromLDAP(env *environment.Environment) ([]User, error) {
 	var users []User
-	ad, err := GetADConfig()
+	cfg, err := readLDAPConfig(env)
 	if err != nil {
-		return users, err
+		return nil, err
 	}
+
 	//连接活动目录
-	var ldapServer = ad.LdapServer
-	port, err := strconv.ParseInt(ad.LdapPort, 10, 16)
-	var ldapPort = port
-
-	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", ldapServer, ldapPort))
-
+	l, err := ldap.Dial("tcp", cfg.Address)
 	if err != nil {
 		return users, err
 	}
-
 	defer l.Close()
 
-	err = l.Bind(ad.Username, ad.Password)
-
+	err = l.Bind(cfg.Username, cfg.Password)
 	if err != nil {
-
 		return users, err
 	}
 
 	searchRequest := ldap.NewSearchRequest(
-		ad.BaseDN,
+		cfg.BaseDN,
 		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
 		"(&(objectClass=organizationalPerson))",
 		[]string{},
@@ -66,41 +64,30 @@ func GetUser() ([]User, error) {
 
 	//获取数据
 	sr, err := l.Search(searchRequest)
-
 	if err != nil {
 		return users, err
 	}
 
 	for i := 0; i < len(sr.Entries); i++ {
-		var user User
-		user.Name = sr.Entries[i].GetAttributeValue("name")
-		user.Description = sr.Entries[i].GetAttributeValue("description")
-		user.Attributes["streetAddress"] = sr.Entries[i].GetAttributeValue("streetAddress")
-		user.Attributes["company"] = sr.Entries[i].GetAttributeValue("company")
-		user.Attributes["telephoneNumber"] = sr.Entries[i].GetAttributeValue("telephoneNumber")
-		user.Source = "AD"
-		created := strings.Split(sr.Entries[i].GetAttributeValue("whenCreated"), ".")[0]
-		changed := strings.Split(sr.Entries[i].GetAttributeValue("whenChanged"), ".")[0]
-		user.CreatedAt = changeTime(created)
-		user.UpdatedAt = changeTime(changed)
-		users = append(users, user)
+		users = append(users, User{
+			Name:        sr.Entries[i].GetAttributeValue("name"),
+			Description: sr.Entries[i].GetAttributeValue("description"),
+			Attributes: map[string]interface{}{
+				"streetAddress":   sr.Entries[i].GetAttributeValue("streetAddress"),
+				"company":         sr.Entries[i].GetAttributeValue("company"),
+				"telephoneNumber": sr.Entries[i].GetAttributeValue("telephoneNumber")},
+			Source:    "AD",
+			CreatedAt: convertToTime(strings.Split(sr.Entries[i].GetAttributeValue("whenCreated"), ".")[0]),
+			UpdatedAt: convertToTime(strings.Split(sr.Entries[i].GetAttributeValue("whenChanged"), ".")[0]),
+		})
 	}
 	return users, nil
 }
 
-func changeTime(str string) time.Time {
-	y := str[:4]
-	m := str[4:6]
-	d := str[6:8]
-	h := str[8:10]
-	mi := str[10:12]
-	s := str[12:]
-	time1 := strings.Join([]string{y, m, d}, "-")
-	time2 := strings.Join([]string{h, mi, s}, ":")
-	toBeCharge := strings.Join([]string{time1, time2}, " ")
-	timeLayout := "2006-01-02 15:04:05"
-	loc, _ := time.LoadLocation("Local")
-	theTime, _ := time.ParseInLocation(timeLayout, toBeCharge, loc)
-	hh, _ := time.ParseDuration("1h")
-	return theTime.Add(8 * hh)
+func convertToTime(str string) time.Time {
+	if len(str) > 12 {
+		return time.Time{}
+	}
+	theTime, _ := time.ParseInLocation("20060102150405", str, time.Local)
+	return theTime
 }
