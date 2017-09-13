6845594ebf8f464ddacfea96574c3a4c98b6faed
meifakun <runner.mei@gmail.com>
2017-09-13 10:38:18 +0800

    [util] add CloseWith()

--- util/util.go
+++ util/util.go
@@ -1,10 +1,13 @@
 package util
 
-import "log"
+import (
+	"io"
+	"log"
+)
 
-// CatchError 捕获错误并打印
-func CatchError(err error) {
-	if err != nil {
+// CloseWith 捕获错误并打印
+func CloseWith(closer io.Closer) {
+	if err := closer.Close(); err != nil {
 		log.Println("[WARN]", err)
 	}
 }
