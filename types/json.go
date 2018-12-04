b56bc404860896f4a2725a012b658800ca1f709a
eeiceyes <eeiceyes@163.com>
2018-12-04 13:19:00 +0800

    add IsUnmodifiable

--- types/json.go
+++ types/json.go
@@ -20,19 +20,20 @@ type ClassSpec struct {
 }
 
 type FieldSpec struct {
-	Name         string                 `json:"name" ymal:"name"`
-	Label        string                 `json:"label,omitempty" ymal:"label,omitempty"`
-	Description  string                 `json:"description,omitempty" yaml:"description,omitempty"`
-	Type         string                 `json:"type" yaml:"type"`
-	IsArray      bool                   `json:"is_array,omitempty" yaml:"is_array,omitempty"`
-	IsEmbedded   bool                   `json:"embedded,omitempty" yaml:"embedded,omitempty"`
-	IsRequired   bool                   `json:"required,omitempty" yaml:"required,omitempty"`
-	IsReadOnly   bool                   `json:"readonly,omitempty" yaml:"readonly,omitempty"`
-	IsUniquely   bool                   `json:"unique,omitempty" yaml:"unique,omitempty"`
-	Default      string                 `json:"default,omitempty" yaml:"default,omitempty"`
-	Unit         string                 `json:"unit,omitempty" yaml:"unit,omitempty"`
-	Restrictions *RestrictionSpec       `json:"restrictions,omitempty" yaml:"restrictions,omitempty"`
-	Annotations  map[string]interface{} `json:"annotations,omitempty" yaml:"annotations,omitempty"`
+	Name         		string                 `json:"name" ymal:"name"`
+	Label        		string                 `json:"label,omitempty" ymal:"label,omitempty"`
+	Description  		string                 `json:"description,omitempty" yaml:"description,omitempty"`
+	Type         		string                 `json:"type" yaml:"type"`
+	IsArray      		bool                   `json:"is_array,omitempty" yaml:"is_array,omitempty"`
+	IsEmbedded   		bool                   `json:"embedded,omitempty" yaml:"embedded,omitempty"`
+	IsRequired   		bool                   `json:"required,omitempty" yaml:"required,omitempty"`
+	IsReadOnly   		bool                   `json:"readonly,omitempty" yaml:"readonly,omitempty"`
+	IsUniquely   		bool                   `json:"unique,omitempty" yaml:"unique,omitempty"`
+	IsUnmodifiable 	bool                   `json:"unmodifiable,omitempty" yaml:"unmodifiable,omitempty"`
+	Default      		string                 `json:"default,omitempty" yaml:"default,omitempty"`
+	Unit         		string                 `json:"unit,omitempty" yaml:"unit,omitempty"`
+	Restrictions 		*RestrictionSpec       `json:"restrictions,omitempty" yaml:"restrictions,omitempty"`
+	Annotations  		map[string]interface{} `json:"annotations,omitempty" yaml:"annotations,omitempty"`
 }
 
 type RestrictionSpec struct {
