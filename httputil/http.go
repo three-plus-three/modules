05ac0338ab66ff1cdfbd44d67edc89e49fa93025
meifakun <runner.mei@gmail.com>
2019-02-27 17:26:50 +0800

    [httputil] add RealIP()

--- httputil/http.go
+++ httputil/http.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"strings"
 
@@ -195,4 +196,21 @@ func Delete(url string, body, result interface{}, cachedBuffer *bytes.Buffer) er
 
 func Do(method, url string, body, statusCode int, result interface{}, cachedBuffer *bytes.Buffer) errors.RuntimeError {
 	return InvokeHttp(method, url, body, statusCode, result, cachedBuffer)
+}
+
+const (
+	HeaderXForwardedFor = "X-Forwarded-For"
+	HeaderXRealIP       = "X-Real-IP"
+)
+
+func RealIP(req *http.Request) string {
+	ra := req.RemoteAddr
+	if ip := req.Header.Get(HeaderXForwardedFor); ip != "" {
+		ra = ip
+	} else if ip := req.Header.Get(HeaderXRealIP); ip != "" {
+		ra = ip
+	} else {
+		ra, _, _ = net.SplitHostPort(ra)
+	}
+	return ra
 }
