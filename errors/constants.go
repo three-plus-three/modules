a54fd9d341f66a0632da00122940fb1e30067630
meifakun <runner.mei@gmail.com>
2019-04-11 19:12:28 +0800

    add errors.ErrCodePending2

--- errors/constants.go
+++ errors/constants.go
@@ -6,13 +6,15 @@ import (
 
 // 错误码
 const (
-	ErrCodeTimeout          = http.StatusGatewayTimeout*1000 + 1
-	ErrCodeNotFound         = http.StatusNotFound * 1000
-	ErrCodeInternalError    = http.StatusInternalServerError*1000 + 1
-	ErrCodeDisabled         = http.StatusForbidden*1000 + 1
-	ErrCodeNotAcceptable    = http.StatusNotAcceptable*1000 + 1
-	ErrCodeNotImplemented   = http.StatusNotImplemented*1000 + 1
+	ErrCodeTimeout        = http.StatusGatewayTimeout*1000 + 1
+	ErrCodeNotFound       = http.StatusNotFound * 1000
+	ErrCodeInternalError  = http.StatusInternalServerError*1000 + 1
+	ErrCodeDisabled       = http.StatusForbidden*1000 + 1
+	ErrCodeNotAcceptable  = http.StatusNotAcceptable*1000 + 1
+	ErrCodeNotImplemented = http.StatusNotImplemented*1000 + 1
+	//  StatusResetContent 不可使用，有问题
 	ErrCodePending          = http.StatusResetContent*1000 + 1
+	ErrCodePending2         = 570*1000 + 1
 	ErrCodeIsRequired       = http.StatusBadRequest*1000 + 900
 	ErrCodeNetworkError     = 560000
 	ErrCodeInterruptError   = 561000
