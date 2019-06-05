284f9cdf4d87290c9cf3018a0a9185af2565c36f
meifakun <runner.mei@gmail.com>
2019-06-05 19:09:08 +0800

    [util] add UnmarshalFromHjson

--- util/hjson.go
+++ util/hjson.go
@@ -30,6 +30,14 @@ func HjsonToJSON(bs []byte) ([]byte, error) {
 	return fixJSON(out), nil
 }
 
+func UnmarshalFromHjson(in []byte, value interface{}) error {
+	bs, err := HjsonToJSON(in)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bs, value)
+}
+
 func FromHjsonFile(filename string, target interface{}) error {
 	bs, err := ioutil.ReadFile(filename)
 	if err != nil {
